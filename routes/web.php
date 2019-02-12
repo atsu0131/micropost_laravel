55d65afe40fa15f7e7eced391206393770f2f8bc
EC2 Default User <ec2-user@ip-172-31-32-193.us-east-2.compute.internal>
2019-02-12 15:02:37 +0000

    repair

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@
 
 Route::get('/', 'MicropostsController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
